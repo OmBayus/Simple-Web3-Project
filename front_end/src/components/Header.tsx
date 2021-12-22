232d7ac1164c40da033df459aad224d4e1b0fe3d
OmBayus <omerbayramcavus@gmail.com>
2021-12-22 14:05:50 +0300

    Setup network

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -3,6 +3,7 @@ import {useEthers} from "@usedapp/core"
 import {Button, Grid} from "@mui/material"
 import { makeStyles } from "@mui/styles"
 import {useNavigate} from "react-router-dom"
+import {providers} from "ethers"
 
 
 const useStyles = makeStyles(({
@@ -19,6 +20,21 @@ const useStyles = makeStyles(({
     }
 }))
 
+declare let window: any;
+
+const setupNetwork = async () => {
+    try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: `0x${parseInt("42", 10).toString(16)}` }],
+        });
+      } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        console.log(switchError)
+        // handle other "switch" errors
+      }
+}
+
 const Header = ()=>{
 
     const classes = useStyles()
@@ -28,6 +44,16 @@ const Header = ()=>{
     const {account,activateBrowserWallet,deactivate} = useEthers()
 
     const isConnected = account !== undefined
+
+    const provider = new providers.Web3Provider(window.ethereum);
+
+    const connectWallet = async()=>{
+        const { chainId } = await provider.getNetwork()
+        if(chainId !== 42){
+            await setupNetwork()
+        }
+        activateBrowserWallet()
+    }
 
 
     return(
@@ -43,7 +69,7 @@ const Header = ()=>{
                     {isConnected && <Button variant="contained" onClick={()=>navigate("/setnumber")}>Set your Number</Button>}
                     {isConnected ? 
                     <Button variant="contained" color="warning" onClick={deactivate}>{account}</Button> :
-                    <Button variant="contained" onClick={()=>activateBrowserWallet()}>Connect</Button>  }
+                    <Button variant="contained" onClick={connectWallet}>Connect</Button>  }
                 </div>
             </div>
         </Grid>
